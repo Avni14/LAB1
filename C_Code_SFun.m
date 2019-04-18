ac7c0ee79cd7f691b8ad09310d5a09ac296a6a84
Avni14 <46839930+Avni14@users.noreply.github.com>
2019-04-18 09:55:10 +0530

    Delete C_Code_SFun.m

--- C_Code_SFun.m
+++
@@ -1,9 +0,0 @@
- % C Code to S-function 
- clc,clear;
-def=legacy_code('initialize')
-def.SFunctionName='A3_051';
-def.OutputFcnSpec='double y1=add_2(double u1, double u2)';
-def.HeaderFiles={'add_lib.h'};
-def.SourceFiles={'main.c'};
-legacy_code('sfcn_cmex_generate',def)
-legacy_code('compile',def)
